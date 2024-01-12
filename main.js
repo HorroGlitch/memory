eb8aa57210703479cc3550e69d6ab677c468b916
HorroGlitch <lverot@edenschool.fr>
2024-01-12 15:42:17 +0100

    can choose only 2 cards

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 import './style.css'
 
 let cards = document.querySelectorAll(".cards");
+let user_returned = 0;
+let game_returned = 0;
 
 for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', ()=>{
-        console.log("hello");
-    })
-    
+        if (user_returned < 2) {
+            cards[i].style.backgroundColor = "white"
+            user_returned ++;
+        }
+    });
 }
