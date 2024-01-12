89509d57ebf7f85bf2051dd98bacbabb16a8fd45
HorroGlitch <lverot@edenschool.fr>
2024-01-12 13:49:51 +0100

    my cards are activate

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 import './style.css'
 
+let cards = document.querySelectorAll(".cards");
+
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', ()=>{
+        console.log("hello");
+    })
+    
+}
